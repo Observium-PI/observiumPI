bcad8914038c41c65b522cda257039e91206e2f5
Vinifd1234 <Vinicius.pedroso@Bandtec.com.br>
2022-04-10 17:49:53 -0300

    Consertando conflito com commit antigo, cards dos computadores sendo novamente adicionados junto aos icones de exclusão e alteração de informações

--- public/js/backendTelaDeMaquinas.js
+++ public/js/backendTelaDeMaquinas.js
@@ -12,40 +12,68 @@ if (sessionStorage.getItem("logado") == false || sessionStorage.getItem("logado"
                 dados.forEach(element => {
                     if (element.sistemaOperacional == "Windows") {
                         element.imagemSO = "./assets/imgs/monitor-windows.png"
+                        element.img_edit = "./assets/imgs/editar.png"
+                        element.img_delete = "./assets/imgs/garbage.png"
                         const container = document.getElementById("container")
                         let card = document.createElement("div")
                         let tituloMaquina = document.createElement("h3")
                         let divSO = document.createElement("div")
+                        let div_edit_delete = document.createElement("div")
+                        let img_edit = document.createElement("div")
+                        let img_delete = document.createElement("div")
+
 
                         tituloMaquina.innerHTML += element.nome;
 
                         container.appendChild(card)
                         card.appendChild(divSO)
                         card.appendChild(tituloMaquina)
+                        card.appendChild(div_edit_delete)
+                        div_edit_delete.appendChild(img_edit)
+                        div_edit_delete.appendChild(img_delete)
 
                         tituloMaquina.classList.add("hostname")
                         card.classList.add("item_container")
                         divSO.classList.add("item_container_SO")
+                        div_edit_delete.classList.add("div_edit_delete")
+                        img_edit.classList.add("item_container_img_edit")
+                        img_delete.classList.add("item_container_img_delete")
 
                         divSO.style.backgroundImage = "url(" + element.imagemSO + ")"
+                        img_edit.style.backgroundImage = "url(" + element.img_edit + ")"
+                        img_delete.style.backgroundImage = "url(" + element.img_delete + ")"
                     } else {
                         element.imagemSO = "./assets/imgs/monitor-linux.png"
+                        element.img_edit = "./assets/imgs/editar.png"
+                        element.img_delete = "./assets/imgs/garbage.png"
                         const container = document.getElementById("container")
                         let card = document.createElement("div")
                         let tituloMaquina = document.createElement("h3")
                         let divSO = document.createElement("div")
+                        let div_edit_delete = document.createElement("div")
+                        let img_edit = document.createElement("div")
+                        let img_delete = document.createElement("div")
+
 
                         tituloMaquina.innerHTML += element.nome;
 
                         container.appendChild(card)
                         card.appendChild(divSO)
                         card.appendChild(tituloMaquina)
+                        card.appendChild(div_edit_delete)
+                        div_edit_delete.appendChild(img_edit)
+                        div_edit_delete.appendChild(img_delete)
 
                         tituloMaquina.classList.add("hostname")
                         card.classList.add("item_container")
                         divSO.classList.add("item_container_SO")
+                        div_edit_delete.classList.add("div_edit_delete")
+                        img_edit.classList.add("item_container_img_edit")
+                        img_delete.classList.add("item_container_img_delete")
 
                         divSO.style.backgroundImage = "url(" + element.imagemSO + ")"
+                        img_edit.style.backgroundImage = "url(" + element.img_edit + ")"
+                        img_delete.style.backgroundImage = "url(" + element.img_delete + ")"
                     }
                     console.log("Oi: " + element.nome);
 
